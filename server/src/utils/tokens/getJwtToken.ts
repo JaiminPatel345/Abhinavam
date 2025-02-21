cebfaa2d5bb78919919aab5f2504377128f0ec3d
Jaimin Detroja <124363976+JaiminPatel345@users.noreply.github.com>
2025-02-21 11:33:46 +0530

    remove redis , change token time , add jti

--- server/src/utils/tokens/getJwtToken.ts
+++ server/src/utils/tokens/getJwtToken.ts
@@ -1,18 +1,28 @@
 import jwt from 'jsonwebtoken';
-import {v4 as uuidv4} from 'uuid';
-import {client} from "../../redis/redis.js";
+
+//TODO: when need advance security
+// export const generateAccessToken = async (userId: string): Promise<string> => {
+//   const uuid = uuidv4();
+//
+//   await client.set(uuid, userId, {
+//     EX: 60 * 30 //30 min
+//   });
+//
+//   return jwt.sign(
+//       {uuid},
+//       process.env.JWT_ACCESS_SECRET as string,
+//       {expiresIn: '30Minutes'}
+//   );
+// };
 
 export const generateAccessToken = async (userId: string): Promise<string> => {
-  const uuid = uuidv4();
-
-  await client.set(uuid, userId, {
-    EX: 60 * 30 //30 min
-  });
-
   return jwt.sign(
-      {uuid},
+      {
+        userId,
+        jti: crypto.randomUUID()
+      },
       process.env.JWT_ACCESS_SECRET as string,
-      {expiresIn: '30Minutes'}
+      {expiresIn: '20Minutes'}
   );
 };
 
@@ -20,7 +30,7 @@ export const generateRefreshToken = (userId: string): string => {
   return jwt.sign(
       {userId},
       process.env.JWT_REFRESH_SECRET as string,
-      {expiresIn: '4Weeks'}
+      {expiresIn: '8Weeks'}
   );
 };
 
